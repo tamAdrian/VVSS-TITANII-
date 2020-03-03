acbc2246f23fd428a8f5976cbce74699f81c76c9
ionutfrent15 <ionutfrent15@gmail.com>
2020-03-03 13:56:03 +0200

    sonarlint model

--- src/main/java/pizzashop/Main.java
+++ src/main/java/pizzashop/Main.java
@@ -42,8 +42,8 @@ public class Main extends Application {
                 Optional<ButtonType> result = exitAlert.showAndWait();
                 if (result.get() == ButtonType.YES){
                     //Stage stage = (Stage) this.getScene().getWindow();
-                    System.out.println("Incasari cash: "+service.getTotalAmount(PaymentType.Cash));
-                    System.out.println("Incasari card: "+service.getTotalAmount(PaymentType.Card));
+                    System.out.println("Incasari cash: "+service.getTotalAmount(PaymentType.CASH));
+                    System.out.println("Incasari card: "+service.getTotalAmount(PaymentType.CARD));
 
                     primaryStage.close();
                 }
--- src/main/java/pizzashop/service/PaymentAlert.java
+++ src/main/java/pizzashop/service/PaymentAlert.java
@@ -45,10 +45,10 @@ public class PaymentAlert implements PaymentOperation {
         Optional<ButtonType> result = paymentAlert.showAndWait();
         if (result.get() == cardPayment) {
             cardPayment();
-            service.addPayment(tableNumber, PaymentType.Card,totalAmount);
+            service.addPayment(tableNumber, PaymentType.CARD,totalAmount);
         } else if (result.get() == cashPayment) {
             cashPayment();
-            service.addPayment(tableNumber, PaymentType.Cash,totalAmount);
+            service.addPayment(tableNumber, PaymentType.CASH,totalAmount);
         } else if (result.get() == cancel) {
              cancelPayment();
         } else {
